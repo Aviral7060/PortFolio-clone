74d3b5d3d96bd3c475bcc8bf3a0ae8fefb56bcca
aviral jain <21ucs042@gmail.com>
2024-06-07 21:31:12 +0530

    Portfolio clone

---
+++ index.js
@@ -0,0 +1,12 @@
+let daynight=document.querySelector('.daynight');
+let banner=document.querySelector('.banner');
+daynight.addEventListener('click',()=>{
+    banner.classList.toggle('night');
+})
+
+let typingEffect =  new Typed('#text',{
+    strings:["SportXIT","SportsEnthusiast","coder"],loop:true,typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000
+
+});
